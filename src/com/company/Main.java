1593ec81cb4b603ef0813c3a42516cab1f7472d4
Nuray <nbakyt90@gmail.com>
2021-03-07 23:51:12 +0600

    First commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import com.company.controllers.FlightController;
+import com.company.data.PostgresDB;
+import com.company.data.interfaces.IDB;
+import com.company.repositories.FlightRepositories;
+import com.company.repositories.interfaces.IFlightRepositories;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        IDB db = new PostgresDB();
+        IFlightRepositories repo = new FlightRepositories(db);
+        FlightController controller = new FlightController(repo);
+        MyApplication app = new MyApplication(controller);
+        app.start();
     }
 }
